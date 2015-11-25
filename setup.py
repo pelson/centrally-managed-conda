c336cb4124a1b06729131f581c5edb99a65e3b4b
Phil Elson <pelson.pub@gmail.com>
2015-11-25 14:08:38 +0000

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import versioneer
+
+
+setup(
+      name='centrally-managed-conda',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='Tools and documentation for deploying centrally managed conda environments.',
+      author='Phil Elson',
+      author_email='pelson.pub@gmail.com',
+      url='https://github.com/pelson/centrally-managed-conda',
+      packages=['centrally_managed_conda'],
+      entry_points={
+          'console_scripts': [
+              'fetch-conda-recipes = centrally-managed-conda.fetch_recipes:main',
+          ]
+      },
+     )
+
